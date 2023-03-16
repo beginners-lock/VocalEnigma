b58f082fa940a6fde2140f72cfc2e7a0b790c2eb
beginners-lock <chukwurophi@gmail.com>
2023-03-16 11:09:45 +0100

    'first'

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let err = 2;
 
 window.addEventListener('load', function(){
+	//Get and set heights
+	let height = window.innerHeight;
+	/*if(window.innerWidth<=500){
+		document.getElementById('homeleft').style.minHeight = 0.40*height;
+		document.getElementById('homeleft').style.maxHeight = 0.40*height;
+		document.getElementById('homeright').style.minHeight = 0.38*height;
+		document.getElementById('homeright').style.maxHeight = 0.38*height;
+	}*/
+
+	if(window.innerWidth>=768 || window.innerWidth<=1023){
+		document.getElementById('homeleft').style.minHeight = 0.40*height;
+		document.getElementById('homeleft').style.maxHeight = 0.40*height;
+		document.getElementById('homeright').style.minHeight = 0.38*height;
+		document.getElementById('homeright').style.maxHeight = 0.38*height;
+	}
+	
 	let datetime = new Date();
 	let time = datetime.toString().split(' ')[4];
 	let hour = time.toString().split(':')[0]
