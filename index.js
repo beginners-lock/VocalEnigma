ef4f88ef5d0023a633634d32de42483613f31216
beginners-lock <chukwurophi@gmail.com>
2023-04-24 21:52:14 +0100

    first

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let gender = 'Female';
+let accent = 'UK English';
+let volume = 0.5;
+let rate = 1;
+let pitch = 1;
+
 let err = 2;
 
 window.addEventListener('load', function(){
@@ -35,9 +41,42 @@ window.addEventListener('load', function(){
 		document.getElementById('mydiv').style.width = '0px';
 	});
 
-	let opening = 'Welcome to VocalEnigma. One of the portfolio sites of beginners lock which makes use of ResponsiveVoice API. Type something in, then test and I\'ll do the rest'
+	document.getElementById('textarea').addEventListener('focus', ()=>{
+		if(document.getElementById('settingspane').style.top =="5vh"){
+			document.getElementById('settingspane').style.top = '110vh';
+		}
+	});
+
+	document.getElementById('textarea').addEventListener('keypress', (e)=>{
+		if(e.key == "Enter"){ 
+			animate();
+			let text = document.getElementById('textarea').value.trim();
+			let voice = accent+' '+gender;
+			if(text != ''){
+				responsiveVoice.speak(text, voice, { volume: volume, rate: rate, pitch:pitch, onend: ()=>{
+					document.getElementById('micdiv').style.display = 'none';
+					document.getElementById('tadiv').style.display = 'flex';
+				} });
+			}
+		}
+	});
+
+	document.getElementById('micdiv').addEventListener('click', ()=>{undo();});
+
+	let opening = 'Welcome to VocalEnigma by beginners lock. Anything you\'d like me to say?'
 	responsiveVoice.speak(opening, 'UK English Female')
 });
+
+function animate(){
+	document.getElementById('textarea').value = '';
+	document.getElementById('tadiv').style.display = 'none';
+	document.getElementById('micdiv').style.display = 'flex';
+}
+
+function undo(){
+	document.getElementById('micdiv').style.display = 'none';
+	document.getElementById('tadiv').style.display = 'flex';
+}
 
 function toggle(){
 	//Laptop
@@ -100,13 +139,49 @@ function theme(type){
 	}
 }
 
+function setgender(el){
+	gender = el.nextElementSibling.innerText;
+}
+
+function setaccent(el){
+	accent = el.nextElementSibling.innerText;
+}
+
+function setvocal(el, globvar){
+	if(globvar == 'volume'){
+		volume = parseFloat(el.value/10).toFixed(1);
+	}
+
+	if(globvar == 'pitch'){
+		pitch = parseFloat((el.value/10)*2).toFixed(1);
+	}
+
+	if(globvar == 'rate'){
+		rate = parseFloat((el.value/10) * 1.5).toFixed(1);
+	}
+}
+
+function docker(){
+	let top = document.getElementById('settingspane').style.top;
+	if(top=="" || top=="110vh"){
+		if(window.innerWidth<=500){ document.getElementById('settingspane').style.top = '8vh'; }
+
+		if(window.innerWidth>=768 && window.innerWidth<=1023){ document.getElementById('settingspane').style.top = '5vh'; }
+
+		if(window.innerWidth>=1024){ document.getElementById('settingspane').style.top = '5vh'; }
+	}else{
+		document.getElementById('settingspane').style.top = '110vh';
+	}
+}
+
 function speak(){
 	let text = document.getElementById('textarea').value.trim();
 	if(!(text == '')){
-		responsiveVoice.speak(text);
+		responsiveVoice.speak(text, 'Italian Female');
 	}else{
 		if(err>=0){
 			document.getElementById('textarea').focus();
+			responsiveVoice.speak("Type something in first dammit. Gaawwwwd I hate this job!!!", 'UK English Female', {volume:10});
 			err--;
 		}else{
 			responsiveVoice.speak("Type something in first dammit. Gaawwwwd I hate this job!!!", 'UK English Female', {volume:10});
